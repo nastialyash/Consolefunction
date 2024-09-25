c406b28b520ed1320acca2e4988f979cee37ddeb
nastialyash <user251@school22.rv.ua>
2024-09-25 20:12:04 +0300

    Добавьте файлы проекта.

---
+++ Consolefunction.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+    void sumRange(int a, int b, int result)
+    {
+        result = 0;  
+        
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        for (int i = a; i <= b; ++i) {
+            result += i;
+        }
+    }
+
+    int main() {
+        int num1, num2, result;
+
+       cout << "Enter first num :  ";
+        cin >> num1;
+        cout << "Enter second num : ";
+        cin >> num2;
+
+      
+        sumRange(num1, num2, result);
+
+    
+        cout << "Sum of numbers " << num1 << " і " << num2 << " = " << result << endl;
+
+
+
+        int base;     
+        int num2;    
+        int result = 1; 
+
+     
+        cout << "Enter the base : ";
+        cin >> base;
+        cout << "Enter step of num: ";
+        cin >> num2;
+
+        for (int i = 0; i < num2; ++i) {
+            result *= base;
+        }
+
+       
+        cout << base <<   num2 << " = " << result << endl;
+        
+
+
+
+
+}
+
